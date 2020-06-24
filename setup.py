45c9787a4792e035459bfb1656290682a07ef70c
Akatuoro <akatuoro6@gmail.com>
2020-06-24 18:44:51 +0200

    make package installable with pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tranX", # Replace with your own username
+    version="0.0.1",
+    author="pcyin",
+    author_email="",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pcyin/tranX",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
